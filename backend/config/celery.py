6bcb659b7a652d0daf31e8167a9c671271620fbb
usik-sviatoslav <usik.sviatoslav.dev@gmail.com>
2024-07-23 14:24:26 +0300

    Updated loggers for production mode & defined loggers in celery config

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 
+import logging.config
 import os
 from datetime import timedelta
 
@@ -15,8 +16,20 @@ settings.CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseSchedule
 
 app = Celery("backend", broker="redis://redis:6379/0", backend="redis://redis:6379/0")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.broker_connection_retry_on_startup = True
 app.autodiscover_tasks()
 
+if not settings.DEBUG:
+    logging.config.dictConfig(settings.LOGGING)
+
+# Defining loggers
+celery_worker_logger = logging.getLogger("celery.worker")
+celery_beat_logger = logging.getLogger("celery.beat")
+
+# Exporting loggers
+__all__ = ["celery_worker_logger", "celery_beat_logger"]
+
+# Defining celery beat schedule
 app.conf.beat_schedule = {
     "clean_blacklist": {"task": "apps.user_auth.jwt_auth.tasks.clean_blacklist", "schedule": timedelta(days=1)},
 }
--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -13,32 +13,37 @@ SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
+
+def create_file_logger(path_to_file):
+    return {
+        "level": "ERROR",
+        "class": "logging.TimedRotatingFileHandler",
+        "filename": "/var/log/backend/" + path_to_file,
+        "when": "midnight",
+        "interval": 1,
+        "backupCount": 10,
+        "formatter": "verbose",
+    }
+
+
 # Logs
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "handlers": {
-        "file": {
-            "level": "ERROR",
-            "class": "logging.FileHandler",
-            "filename": os.path.join(BASE_DIR, "logs", "django_error.log"),  # noqa: F405
-        },
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "verbose",
-        },
+        "django_file": create_file_logger("django/django_errors.log"),
+        "celery_worker_file": create_file_logger("celery/worker/celery_worker_errors.log"),
+        "celery_beat_file": create_file_logger("celery/beat/celery_beat_errors.log"),
+    },
+    "loggers": {
+        "django": {"handlers": ["django_file"], "level": "ERROR", "propagate": True},
+        "celery.worker": {"handlers": ["celery_worker_file"], "level": "ERROR", "propagate": True},
+        "celery.beat": {"handlers": ["celery_beat_file"], "level": "ERROR", "propagate": True},
     },
     "formatters": {
         "verbose": {
             "format": "{levelname} {asctime} {module} {message}",
             "style": "{",
-        },
-    },
-    "loggers": {
-        "django": {
-            "handlers": ["file", "console"],
-            "level": "ERROR",
-            "propagate": True,
         },
     },
 }
