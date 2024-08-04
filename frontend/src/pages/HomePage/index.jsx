ee40822b8de2b7841c93e8a3e6aba1495362c18e
Tatiana Zaidel <8tzaidel@gmail.com>
2024-08-04 13:16:56 +0200

    add hero section

--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -1,12 +1,14 @@
 import Header from '../../components/Header';
+import Hero from '../../components/Hero';
 import Footer from '../../components/Footer';
 
 import styles from './styles.module.css';
 
-export default function CartPage() {
+export default function HomePage() {
   return (
     <>
       <Header />
+      <Hero />
       <Footer />
     </>
   );
