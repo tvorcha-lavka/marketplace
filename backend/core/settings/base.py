20396402a9c8b77bcd1c280c56d3c901801a33e7
usik-sviatoslav <usik.sviatoslav.dev@gmail.com>
2024-08-09 20:18:36 +0300

    Update .env.example, celery & redis settings

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -84,7 +84,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1",
+        "LOCATION": os.getenv("REDIS_BROKER_URL", "redis://redis:6379/1"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
