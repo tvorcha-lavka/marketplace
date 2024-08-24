2d8244a5f0c3af9860235062b294d92ed4f85bb0
usik-sviatoslav <usik.sviatoslav.dev@gmail.com>
2024-08-24 08:16:25 +0300

    Update conftest

--- backend/apps/category/tests/conftest.py
+++ backend/apps/category/tests/conftest.py
@@ -1,11 +1,11 @@
 import pytest
 from django.core.management import call_command
 
-from core.conftest import UserSchema, api_client, auth_client, django_db_setup, users  # noqa: F401
+from core.conftest import UserSchema, api_client, auth_client, users  # noqa: F401
 
 
 # ----- Categories Fixtures --------------------------------------------------------------------------------------------
 @pytest.fixture(scope="session")
-def migrate_categories(django_db_setup, django_db_blocker):  # noqa: F811
+def migrate_categories(django_db_setup, django_db_blocker):  # noqa: django_db_setup
     with django_db_blocker.unblock():
         call_command("migrate_categories")
