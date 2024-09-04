0c8a9037fdb561deaf3f92ef9e6be3809adf69dd
Yanat58 <tanyalyt80@gmail.com>
2024-09-04 22:55:57 +0300

    update App.js, main.js and added two section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,24 @@
 import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import HomePage from '../src/pages/HomePage';
 import CartPage from '../src/pages/CartPage';
 import LoginPage from '../src/pages/LoginPage';
 import RegisterPage from '../src/pages/RegisterPage';
+import SharedLayout from './components/SharedLayout/SharedLayout';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegisterPage />} />
         {/* <Route path="*" element={<NotFoundPage />} /> */}
-      </Routes>
-    </BrowserRouter>
+      </Route>
+    </Routes>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
