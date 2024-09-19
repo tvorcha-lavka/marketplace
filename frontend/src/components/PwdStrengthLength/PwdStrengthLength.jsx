30e37060b9024078b2d82a77bcabf1c7cb18393c
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-09-20 02:55:36 +0300

    create global styles

--- frontend/src/components/PwdStrengthLength/PwdStrengthLength.jsx
+++ frontend/src/components/PwdStrengthLength/PwdStrengthLength.jsx
@@ -20,8 +20,20 @@ export const PwdStrengthLength = ({ strengthLabel }) => {
 export const getStrengthLabel = (password) => {
   const length = password.length;
   if (length < 8)
-    return { label: 'Слабкий пароль', color: '#EA1119', lines: 1 };
+    return {
+      label: 'Слабкий пароль',
+      color: 'var(--error-red)',
+      lines: 1,
+    };
   if (length < 12)
-    return { label: 'Помірно складний пароль', color: '#E77034', lines: 2 };
-  return { label: 'Надійний пароль', color: '#4D9C63', lines: 3 };
+    return {
+      label: 'Помірно складний пароль',
+      color: 'var(--warning-orange)',
+      lines: 2,
+    };
+  return {
+    label: 'Надійний пароль',
+    color: 'var(--success-green)',
+    lines: 3,
+  };
 };
