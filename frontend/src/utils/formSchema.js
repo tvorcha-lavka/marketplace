dce394a8e0cbb96eaf69f5f77dfaa4bd1cb25ef3
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-09-25 15:10:57 +0300

    change form schemas

--- frontend/src/utils/formSchema.js
+++ frontend/src/utils/formSchema.js
@@ -12,47 +12,12 @@ export const codeSchema = Yup.object({
 });
 
 export const forgotPasswordSchema = Yup.object({
-  emailOrPhone: Yup.string()
-    .required()
-    .test(function (value) {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      const uaPhoneRegex = /^\+380[0-9]{9}$/;
-      const plPhoneRegex = /^\+48[0-9]{9}$/;
-      const dePhoneRegex = /^\+49[1-9][0-9]{1,14}$/;
-      const nlPhoneRegex = /^\+31[0-9]{9}$/;
-      const gbPhoneRegex = /^\+44[1-9][0-9]{9,10}$/;
-
-      return (
-        emailRegex.test(value) ||
-        uaPhoneRegex.test(value) ||
-        plPhoneRegex.test(value) ||
-        dePhoneRegex.test(value) ||
-        nlPhoneRegex.test(value) ||
-        gbPhoneRegex.test(value)
-      );
-    }),
+  email: Yup.string().email().required(),
 });
 
 export const schema = Yup.object({
-  emailOrPhone: Yup.string()
-    .required()
-    .test(function (value) {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      const uaPhoneRegex = /^\+380[0-9]{9}$/;
-      const plPhoneRegex = /^\+48[0-9]{9}$/;
-      const dePhoneRegex = /^\+49[1-9][0-9]{1,14}$/;
-      const nlPhoneRegex = /^\+31[0-9]{9}$/;
-      const gbPhoneRegex = /^\+44[1-9][0-9]{9,10}$/;
-
-      return (
-        emailRegex.test(value) ||
-        uaPhoneRegex.test(value) ||
-        plPhoneRegex.test(value) ||
-        dePhoneRegex.test(value) ||
-        nlPhoneRegex.test(value) ||
-        gbPhoneRegex.test(value)
-      );
-    }),
+  email: Yup.string().email().required(),
   password: Yup.string().min(8).max(30).required(),
-  userRemember: Yup.boolean(),
 });
+
+
