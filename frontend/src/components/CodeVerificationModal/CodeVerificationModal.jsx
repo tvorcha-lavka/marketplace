5bed3174ce1b3fe2fc68f1482edf0300dddcafb9
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-09-25 15:15:56 +0300

    update CodeVerification component

--- frontend/src/components/CodeVerificationModal/CodeVerificationModal.jsx
+++ frontend/src/components/CodeVerificationModal/CodeVerificationModal.jsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { useId, useState } from 'react';
 import { Formik, Field, Form } from 'formik';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { useModal } from '../../hooks/useModal';
+import { selectLoading, selectUser } from '../../redux/auth/selectors';
+import { registerComplete, verifyCode } from '../../redux/auth/operations';
 import FormImgComponent from '../FormImgComponent/FormImgComponent';
+import ResendCodeBtn from '../ResendCodeBtn/ResendCodeBtn';
+import Loader from '../Loader/Loader';
 import { codeSchema } from '../../utils/formSchema';
 import { LuArrowLeft } from 'react-icons/lu';
 import css from './CodeVerificationModal.module.css';
@@ -13,22 +18,16 @@ const CodeVerificationModal = ({ type }) => {
   const [otp, setOtp] = useState(Array(6).fill(''));
   const [authError, setAuthError] = useState(false);
 
+  const isLoading = useSelector(selectLoading);
+  const email = useSelector(selectUser);
+  const dispatch = useDispatch();
   const { openModal } = useModal();
   const id = useId();
 
   const getDescription = () => {
-    if (type === 'verification-register') {
-      return 'На вашу електронну пошту надіслано код пітвердження. Ведіть його нижче,щоб завершити реєстрацію';
-    }
-    if (type === 'verification-reset') {
-      return 'Введіть унікальний 6-значний код, який був висланий на ваш e-mail.';
-    }
-    return '';
-  };
-
-  const fakeAuthCheck = async (code) => {
-    await new Promise((resolve) => setTimeout(resolve, 500));
-    return code === '123456';
+    return type === 'verification-register'
+      ? 'На вашу електронну пошту надіслано код підтвердження. Введіть його нижче, щоб завершити реєстрацію'
+      : 'Введіть унікальний 6-значний код, який був висланий на ваш e-mail';
   };
 
   const handleKeyDown = (e, index) => {
@@ -44,95 +43,138 @@ const CodeVerificationModal = ({ type }) => {
       newOtp[index] = value;
       setOtp(newOtp);
       setFieldValue('code', newOtp);
+
+      if (newOtp.every((digit) => digit !== '')) {
+        handleSubmit({ code: newOtp }, { resetForm: () => {} });
+      }
+
       if (value && index < otp.length - 1) {
         document.getElementById(`${id}-code-${index + 1}`).focus();
       }
     }
   };
 
-  const onSubmit = async (values, actions) => {
-    const isAuthenticated = await fakeAuthCheck(values.code.join(''));
-    console.log('Confirmation code:', values.code.join(''));
-    if (isAuthenticated) {
-      if (type === 'verification-register') {
-        openModal('confirmation-modal', { type });
-      } else if (type === 'verification-reset') {
-        openModal('change-pwd');
-      }
+  const handlePaste = (e, setFieldValue) => {
+    const pasteData = e.clipboardData.getData('text');
+    if (/^\d{6}$/.test(pasteData)) {
+      const newOtp = pasteData.split('').slice(0, 6);
+      setOtp(newOtp);
+      newOtp.forEach((value, index) => {
+        document.getElementById(`${id}-code-${index}`).value = value;
+      });
+      setFieldValue('code', newOtp);
+
+      handleSubmit({ code: newOtp }, { resetForm: () => {} });
+    }
+  };
+
+  const handleSubmit = (values, actions) => {
+    const code = values.code.join('');
+
+    localStorage.setItem('verifyResetCode', code);
+
+    let dispatchAction;
+
+    if (type === 'verification-register') {
+      dispatchAction = registerComplete({ code, email });
+    } else if (type === 'verification-reset') {
+      dispatchAction = verifyCode({ code, email });
     } else {
-      console.error('Невірний код');
-      setAuthError(true);
       return;
     }
-    setAuthError(false);
-    actions.resetForm();
-    setOtp(Array(6).fill(''));
+
+    dispatch(dispatchAction)
+      .unwrap()
+      .then(() => {
+        setAuthError(false);
+        actions.resetForm();
+        setOtp(Array(6).fill(''));
+
+        if (type === 'verification-register') {
+          openModal('confirmation-modal', { type });
+          localStorage.removeItem('ResendRegisterCode');
+          localStorage.removeItem('verifyResetCode');
+        } else if (type === 'verification-reset') {
+          openModal('change-pwd');
+        }
+      })
+      .catch((e) => {
+				setAuthError(true);
+				return thunkAPI.rejectWithValue(e.message);
+      });
   };
 
   const handleBack = () => {
-    if (type === 'verification-register') {
-      openModal('register');
-    } else if (type === 'verification-reset') {
-      openModal('forgot-password');
-    }
+    openModal(
+      type === 'verification-register' ? 'register' : 'forgot-password'
+    );
   };
 
   return (
     <div className={css.container}>
       <FormImgComponent />
-      <div className={css.pageContent}>
-        <div className={css.backLinkWrap}>
-          <button onClick={handleBack} className={css.backLink}>
-            <LuArrowLeft className={css.arrowIcon} /> Повернутись назад
-          </button>
-        </div>
-        <h2 className={css.title}>Введіть код</h2>
-        <p className={css.info}>{getDescription()}</p>
 
-        <Formik
-          initialValues={{ code: otp }}
-          validationSchema={codeSchema}
-          onSubmit={onSubmit}
-        >
-          {({ setFieldValue }) => (
-            <Form className={css.form}>
-              <div>
-                <div className={css.codeInputWrapper}>
-                  {otp.map((value, index) => (
-                    <label key={index} htmlFor={`${id}-code-${index}`}>
-                      <Field
-                        id={`${id}-code-${index}`}
-                        type="text"
-                        maxLength="1"
-                        value={value}
-                        onChange={(e) => handleChange(e, index, setFieldValue)}
-                        onKeyDown={(e) => handleKeyDown(e, index)}
-                        className={clsx(
-                          css.input,
-                          value !== '' && css.filled,
-                          authError && css.inputError
-                        )}
-                        autoComplete="off"
-                      />
-                    </label>
-                  ))}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className={css.pageContent}>
+          <div className={css.backLinkWrap}>
+            <button onClick={handleBack} className={css.backLink}>
+              <LuArrowLeft className={css.arrowIcon} /> Повернутись назад
+            </button>
+          </div>
+          <h2 className={css.title}>Введіть код</h2>
+          <p className={css.info}>{getDescription()}</p>
+
+          <Formik
+            initialValues={{ code: otp }}
+            validationSchema={codeSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ setFieldValue }) => (
+              <Form>
+                <div>
+                  <div
+                    onPaste={(e) => handlePaste(e, setFieldValue)}
+                    className={css.codeInputWrapper}
+                  >
+                    {otp.map((value, index) => (
+                      <label key={index} htmlFor={`${id}-code-${index}`}>
+                        <Field
+                          id={`${id}-code-${index}`}
+                          type="text"
+                          maxLength="1"
+                          value={value}
+                          onChange={(e) =>
+                            handleChange(e, index, setFieldValue)
+                          }
+                          onKeyDown={(e) => handleKeyDown(e, index)}
+                          className={clsx(
+                            css.input,
+                            value !== '' && css.filled,
+                            authError && css.inputError
+                          )}
+                          autoComplete="off"
+                        />
+                      </label>
+                    ))}
+                  </div>
+                  {authError && (
+                    <p className={css.additionalInfo}>
+                      Введено неправильний код. Спробуйте ще раз
+                    </p>
+                  )}
                 </div>
-                {authError && (
-                  <p className={css.additionalInfo}>
-                    Введено неправильний код. Спробуйте ще раз
-                  </p>
-                )}
-              </div>
-              <button type="submit" className={css.submitButton}>
-                Повторно наділасти код
-              </button>
-            </Form>
-          )}
-        </Formik>
-        <Link to="/support" className={css.supportLink}>
-          Потрібна допомога?
-        </Link>
-      </div>
+
+                <ResendCodeBtn type={type} />
+              </Form>
+            )}
+          </Formik>
+          <Link to="/support" className={css.supportLink}>
+            Потрібна допомога?
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
