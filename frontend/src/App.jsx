ed4ff871e6d93255d8a2d6e9008dba34ed5e0983
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-10-17 01:55:53 +0300

    add refreshUser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,39 @@
 import { Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { refreshUser, setAuthHeader } from './redux/auth/operations';
+import {
+  selectAccessToken,
+  selectRefreshing,
+} from './redux/auth/selectors';
 import HomePage from '../src/pages/HomePage/HomePage';
 import CartPage from '../src/pages/CartPage/CartPage';
 import SharedLayout from './components/SharedLayout/SharedLayout';
 import ModalParentComponent from './components/ModalParentComponent/ModalParentComponent';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import SupportPage from './pages/SupportPage/SupportPage';
+import LogoutButton from './components/LogoutButton/LogoutButton';
+import Loader from './components/Loader/Loader';
 
 export default function App() {
-	return (
+  const dispatch = useDispatch();
+  const accessToken = useSelector(selectAccessToken);
+  const isRefresh = useSelector(selectRefreshing);
+
+  useEffect(() => {
+    if (accessToken) {
+      setAuthHeader(accessToken); 
+
+      dispatch(refreshUser());
+    }
+  }, [dispatch]);
+
+  return isRefresh ? (
+    <Loader />
+  ) : (
     <>
+      <LogoutButton />
       <ModalParentComponent />
       <Routes>
         <Route path="/" element={<SharedLayout />}>
@@ -18,7 +43,7 @@ export default function App() {
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
+      <Toaster />
     </>
   );
 }
-
