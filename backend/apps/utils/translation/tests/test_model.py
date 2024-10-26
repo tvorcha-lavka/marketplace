040733ecb8f46dd6256369333ab07537220830f9
usik-sviatoslav <usik.sviatoslav.dev@gmail.com>
2024-10-27 01:43:49 +0300

    Improved translation model
    1. Added Deepl translator server error checking.
    2. Improved translation execution queue for celery workers.
    3. Updated pytests.

--- backend/apps/utils/translation/tests/test_model.py
+++ backend/apps/utils/translation/tests/test_model.py
@@ -1,5 +1,8 @@
+import time
+
 import pytest
 from deep_translator import DeeplTranslator
+from django.db import transaction
 from django.utils.text import slugify
 from parler.models import TranslatableModelMixin
 
@@ -8,36 +11,31 @@ from apps.utils.translation.tasks import translate_fields_task
 from .conftest import DummyModel, NoImplementedDummyModel
 
 
-@pytest.mark.django_db
 class TestAutoTranslatableModel:
     @pytest.fixture(autouse=True)
     def setup(self, mocker):
         self.instance_name = "Test Model"
-        self.translated_name = f"Translated {self.instance_name}"
-
         self.instance = DummyModel(name=self.instance_name)
-        self.mock_mixin_1 = mocker.patch.object(TranslatableModelMixin, "save", return_value=None)
-        self.mock_mixin_2 = mocker.patch.object(TranslatableModelMixin, "create_translation", return_value=None)
 
-        self.mock_task = mocker.patch.object(translate_fields_task, "apply_async")
+        self.translated_name = f"Translated {self.instance_name}"
         self.mock_translate = mocker.patch.object(DeeplTranslator, "translate", return_value=self.translated_name)
 
     def test_str_method(self):
         assert str(self.instance) == self.instance_name
 
-    @pytest.mark.django_db(transaction=True)
-    def test_save(self):
+    def test_save(self, mocker):
+        # Mock methods in `save` methods
+        mock_save = mocker.patch.object(TranslatableModelMixin, "save")
+        mock_generate_slug = mocker.patch.object(self.instance, "generate_slug")
+        mock_on_commit = mocker.patch.object(transaction, "on_commit")
+
+        # Save instance
         self.instance.save()
 
-        # Check that the slug was generated correctly
-        assert self.instance.slug == slugify(self.translated_name)
-
-        # Check that the translation task has been started
-        self.mock_task.assert_called_once_with(
-            args=(self.instance._meta.label, self.instance.pk),  # type: ignore
-            queue="high_priority",
-            priority=10,
-        )
+        # Check that methods are called
+        mock_save.assert_called_once()
+        mock_generate_slug.assert_called_once()
+        mock_on_commit.assert_called_once_with(self.instance.add_translate_task)
 
     def test_field_for_slug_implementation(self):
         # Check that the `field_for_slug` rise NotImplementedError
@@ -48,31 +46,30 @@ class TestAutoTranslatableModel:
         # Check that the `field_for_slug` method returns the expected value
         assert self.instance.field_for_slug() == "name"
 
-    def test_generate_slug(self):
-        self.instance.name = "Slug Test"
+    def test_generate_slug(self, mocker):
+        name = "Slug Test"
+        return_translation = f"Translated {name}"
+        mocker.patch.object(DeeplTranslator, "translate", return_value=return_translation)
+
+        self.instance.name = name
         self.instance.generate_slug()
 
         # Check that the slug was generated correctly
-        assert self.instance.slug == slugify(self.translated_name)
+        assert self.instance.slug == slugify(return_translation)
 
-    def test_add_translate_task(self):
+    def test_add_translate_task(self, mocker):
+        # Mock `apply_async` method
+        mock_task = mocker.patch.object(translate_fields_task, "apply_async")
+
+        # Call `add_translate_task` method
         self.instance.add_translate_task()
 
         # Check that the translation task was called with the correct arguments
-        self.mock_task.assert_called_once_with(
-            args=(self.instance._meta.label, self.instance.pk),  # type: ignore
+        mock_task.assert_called_once_with(
+            args=(self.instance._meta.label, self.instance.pk, self.instance.language_code),  # type: ignore
             queue="high_priority",
             priority=10,
         )
-
-    def test_translate(self):
-        translated_value = self.instance.translate("Test String")
-
-        # Check that the `translate` method has been called
-        self.mock_translate.assert_called_once_with("Test String")
-
-        # Check the result of the translation
-        assert translated_value == self.translated_name
 
     def test_get_translated_fields(self):
         translated_fields = self.instance.get_translated_fields()
@@ -80,9 +77,42 @@ class TestAutoTranslatableModel:
         # Check that the `get_translated_fields` method returns a list of fields
         assert translated_fields == ["name"]
 
-    def test_translate_field(self):
+    def test_translate_field(self, mocker):
+        # Mock `create_translation` method
+        mocker.patch.object(TranslatableModelMixin, "create_translation")
+
         # Translating a specific field
         self.instance.translate_field("name", "Test Name")
 
         # Check that the `translate` method has been called
         self.mock_translate.assert_called()
+
+    def test_translate(self, mocker):
+        # Mock `translate` method in DeeplTranslator
+        mock_translate = mocker.patch.object(DeeplTranslator, "translate")
+        value = "Test String"
+
+        # Call `translate` method
+        self.instance.translate(value)
+
+        # Check that the `translate` method has been called
+        mock_translate.assert_called_once_with(value)
+
+    def test_translate_with_connection_error(self, mocker):
+        mocker.patch.object(time, "sleep", return_value=None)
+        mock_translate = mocker.patch.object(
+            DeeplTranslator, "translate", side_effect=ConnectionError("Failed to connect")
+        )
+
+        with pytest.raises(ConnectionError) as exc_info:
+            self.instance.translate("Test String", self.instance.language_code)
+
+        # Check that the error contains the correct message
+        assert str(exc_info.value) == (
+            "Unsuccessful translation for class (%s). Instance id: (%s). Language code: (%s). "
+            "Failed to connect to the translation service after several attempts."
+            % (self.instance.__class__.__name__, self.instance.pk, self.instance.language_code)
+        )
+
+        # Check that the `translate` method has tried to translate message 5 times
+        assert mock_translate.call_count == 5
--- backend/apps/utils/translation/tests/test_tasks.py
+++ backend/apps/utils/translation/tests/test_tasks.py
@@ -1,31 +1,87 @@
+import pytest
 from deep_translator import DeeplTranslator
+from django.conf import settings
 from parler.models import TranslatableModelMixin
 
-from apps.utils.translation.tasks import translate_fields_task
+from apps.utils.translation.tasks import redis_client, translate_fields_task
 from apps.utils.translation.tests.conftest import DummyModel
 
 
-def test_translate_fields_task(mocker):
-    instance_name = "Test Model"
-    translated_name = f"Translated {instance_name}"
+class TestTranslateTask:
+    @pytest.fixture(autouse=True)
+    def setup(self, mocker):
+        instance_name = "Test Model"
+        self.translated_name = f"Translated {instance_name}"
+        self.instance = DummyModel(id=1, name=instance_name)
 
-    instance = DummyModel(id=1, name=instance_name)
+        self.mock_get_obj = mocker.patch.object(DummyModel.objects, "get", return_value=self.instance)
+        self.mock_translate = mocker.patch.object(DeeplTranslator, "translate", return_value=self.translated_name)
+        self.mock_create_translation = mocker.patch.object(
+            TranslatableModelMixin,
+            "create_translation",
+            return_value=None,
+        )
 
-    mock_get_obj = mocker.patch.object(DummyModel.objects, "get", return_value=instance)
-    mock_create_translation = mocker.patch.object(TranslatableModelMixin, "create_translation", return_value=None)
-    mock_translate = mocker.patch.object(DeeplTranslator, "translate", return_value=translated_name)
+    def test_translate_fields_task_with_unlocked_redis(self, mocker):
+        # Mock Redis client methods
+        mock_redis_setnx = mocker.patch.object(redis_client, "setnx", return_value=True)
+        mock_redis_expire = mocker.patch.object(redis_client, "expire", return_value=True)
+        mock_redis_delete = mocker.patch.object(redis_client, "delete", return_value=True)
 
-    translate_fields_task.apply_async(
-        args=(instance._meta.label, instance.pk),  # type: ignore
-        queue="high_priority",
-        priority=10,
-    )
+        translate_fields_task.apply_async(
+            args=(self.instance._meta.label, self.instance.pk, self.instance.language_code),  # type: ignore
+            queue="high_priority",
+            priority=10,
+        )
 
-    # Check that the `get` method has been called once
-    mock_get_obj.assert_called_once()
+        # Check Redis lock methods
+        lock_key = f"translate_task_lock_{self.instance.pk}"
 
-    # Check that the `create_translation` method has been called
-    mock_create_translation.assert_called()
+        # Check if the Redis lock was set and expired
+        mock_redis_setnx.assert_called_once_with(lock_key, "locked")
+        mock_redis_expire.assert_called_once_with(lock_key, 60)
 
-    # Check that the `translate` method has been called
-    mock_translate.assert_called()
+        # Check if the Redis lock was deleted after processing
+        mock_redis_delete.assert_called_once_with(lock_key)
+
+        # Check that the `get` method has been called once
+        self.mock_get_obj.assert_called_once_with(pk=self.instance.pk)
+
+        # Get a language list
+        languages = [code for (code, lang) in settings.LANGUAGES if code != self.instance.language_code]
+
+        # Check that the `translate` method has been called with instance name
+        expected_calls = [mocker.call(self.instance.name) for _ in range(len(languages))]  # type: ignore
+        self.mock_translate.assert_has_calls(expected_calls, any_order=False)
+        assert self.mock_translate.call_count == len(expected_calls)
+
+        # Check that the `create_translation` method has been called with available languages
+        expected_calls = [mocker.call(lang, name=self.translated_name) for lang in languages]
+        self.mock_create_translation.assert_has_calls(expected_calls, any_order=False)
+        assert self.mock_create_translation.call_count == len(expected_calls)
+
+    def test_translate_fields_task_with_locked_redis(self, mocker):
+        # Mock Redis client to simulate locked redis (setnx returns False)
+        mock_redis_setnx = mocker.patch.object(redis_client, "setnx", return_value=False)
+        mock_redis_expire = mocker.patch.object(redis_client, "expire")
+        mock_redis_delete = mocker.patch.object(redis_client, "delete")
+
+        # Run the task
+        translate_fields_task.apply_async(
+            args=(self.instance._meta.label, self.instance.pk, self.instance.language_code),  # type: ignore
+            queue="high_priority",
+            priority=10,
+        )
+
+        # Check that the `setnx` method has been called and returned False
+        lock_key = f"translate_task_lock_{self.instance.pk}"
+        mock_redis_setnx.assert_called_once_with(lock_key, "locked")
+
+        # Ensure that `expire` and `delete` were never called because the lock was not acquired
+        mock_redis_expire.assert_not_called()
+        mock_redis_delete.assert_not_called()
+
+        # Ensure that no translation methods were called, since the lock was not acquired
+        self.mock_get_obj.assert_not_called()
+        self.mock_create_translation.assert_not_called()
+        self.mock_translate.assert_not_called()
