5a6c01b4424be0246a16dccba68911bb92f25820
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-11-02 01:34:07 +0200

    create not found page

--- frontend/src/pages/NotFoundPage/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-export default function SupportPage() {
-  return <h1>Not Found Page, return to <Link to='/'>Home Page</Link></h1>;
+import css from './NotFoundPage.module.css';
+
+export default function NotFoundPage() {
+  return (
+    <div className={css.errorSection}>
+      <div className={css.container}>
+        <img
+          className={css.imgFirst}
+          src="../../../public/images/errorPage/butterfly-first.png"
+          alt="Butterfly"
+        />
+        <h1 className={css.error}>404</h1>
+        <p className={css.errorText}>
+          Ми вже працюємо над розробкою цієї сторінки.
+        </p>
+        <Link to="/" className={css.link}>
+          Повернутись на головну
+        </Link>
+        <img
+          className={css.imgSecond}
+          src="../../../public/images/errorPage/butterfly-second.png"
+          alt="Butterfly"
+        />
+        <img
+          className={css.imgThird}
+          src="../../../public/images/errorPage/butterfly-third.png"
+          alt="Butterfly"
+        />
+      </div>
+    </div>
+  );
 }
