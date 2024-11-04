4ee1c0aba1669fa3ef68b5ce1d2fa7861a8c2b7f
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-11-05 00:30:10 +0200

    add hook for scroll, rename hook for close modal with esc

--- frontend/src/hooks/useClickEsc.jsx
+++ frontend/src/hooks/useClickEsc.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-export const useClickOutsideAndEsc = (closeModal) => {
+export const useClickEsc = (closeModal) => {
   const modalRef = useRef(null);
 
   useEffect(() => {
