d1d0bf898f5dc2a476947e801033b4d7607cb339
Karlova Tetiana <tanyalyt80@gmail.com>
2024-11-05 13:53:44 +0200

    delete page under-down

--- frontend/src/components/SharedLayout/SharedLayout.jsx
+++ frontend/src/components/SharedLayout/SharedLayout.jsx
@@ -1,14 +1,13 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import css from './SharedLayout.module.css';
 
 export default function SharedLayout() {
-  const location = useLocation();
-  const isUnderDevelopPage = location.pathname === '/under-development'
+  
   return (
-    <div className={isUnderDevelopPage ? `${css.page_container}` : ''}>
+    <div >
       <Header />
       <Suspense fallback={null}>
         <Outlet />
