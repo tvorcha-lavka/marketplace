3336102ed6093e7a38e7731a75f6fcde58949fba
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-11-05 19:29:41 +0200

    create universal vip-adverts component

---
+++ frontend/src/components/VipCartsList/VipCartsList.jsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import { FaRegHeart } from 'react-icons/fa';
+
+import { media } from '../../utils/mediaConfig';
+
+import css from './VipCartsList.module.css';
+
+export default function VipCartsList({ item }) {
+  const handleLikeButtonClick = (e) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <div>
+      <button className={css.likeBtn} onClick={handleLikeButtonClick}>
+        <FaRegHeart />
+      </button>
+      <Link to={`/cart/${item.id}`} className={css.link}>
+        <div className={css.item}>
+          <img
+            className={css.image}
+            src={item.img ? item.img : `${media}/page/404/not-found.png`}
+            alt={item.title}
+          />
+          <h3 className={css.titleVip}>VIP-ОГОЛОШЕННЯ</h3>
+        </div>
+        <p className={css.publicDate}>{item.date}</p>
+        <h1 className={css.cartTitle}>{item.title}</h1>
+        <p className={css.price}>{item.price}</p>
+      </Link>
+    </div>
+  );
+}
