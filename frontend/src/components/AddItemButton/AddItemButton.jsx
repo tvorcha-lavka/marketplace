52ef9fabf0f1e85968aafc24c25223b3256fe7c4
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-11-06 03:48:03 +0200

    remove components

--- frontend/src/components/AddItemButton/AddItemButton.jsx
+++
@@ -1,9 +0,0 @@
-import css from './AddItemButton.module.css';
-
-export default function AddItemButton() {
-  return (
-    <>
-      <button className={css.btn}>Додати товар</button>
-    </>
-  );
-}
