8f6bbf524b5b76391ebb21d1fcd62ee01a330f5e
usik-sviatoslav <usik.sviatoslav.dev@gmail.com>
2024-11-08 19:08:02 +0200

    Fixed code formatting for `product` app and TCP connection for a test database

--- backend/apps/product/receivers.py
+++ backend/apps/product/receivers.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import pre_save, pre_delete
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 
 from apps.product.models import ProductImage
