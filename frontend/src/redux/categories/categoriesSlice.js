e6d4cb45f8fc5c51cb3808fe7552279ec2097786
Karlova Tetiana <tanyalyt80@gmail.com>
2024-11-11 15:14:52 +0200

    took logs

--- frontend/src/redux/categories/categoriesSlice.js
+++ frontend/src/redux/categories/categoriesSlice.js
@@ -1,46 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit';
-import {
-    getAllCategories,
-    getPopCategories,
-    getCategoryById, } from './categoriesOperations';
+import { getAllCategories, getCategoryById } from './categoriesOperations';
 
 const categoriesSlice = createSlice({
-    name: 'categories',
-    initialState: {
-        items: [],
-        popular: [],
-        categoryById: {},
-        loading: false,
-        error: null,
-    },
-    extraReducers: (builder) => {
-        builder
-        .addCase(getAllCategories.pending, (state) => {
-                state.loading = true;  
-                state.error = null;
-            })
-       
-        .addCase(getAllCategories.fulfilled, (state, action) => {
-            state.items = action.payload;
-            state.loading = false;
-            state.error = null;
-        })
-       .addCase(getAllCategories.rejected, (state, action) => {
-           state.error = action.payload;
-           state.loading = false;
-       })
-        .addCase(getPopCategories.fulfilled, (state, action) => {
-            state.popular = action.payload;
-            state.loading = false;
-            state.error = null;
-        })
-        .addCase(getCategoryById.fulfilled, (state, action) => {
+  name: 'categories',
+  initialState: {
+    items: [],
+    categoryById: {},
+    loading: false,
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllCategories.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+
+      .addCase(getAllCategories.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(getAllCategories.rejected, (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
+      })
+      .addCase(getCategoryById.fulfilled, (state, action) => {
         state.categoryById = action.payload;
         state.loading = false;
         state.error = null;
       });
-        
-    },
-})
+  },
+});
 
 export const categoriesReducer = categoriesSlice.reducer;
