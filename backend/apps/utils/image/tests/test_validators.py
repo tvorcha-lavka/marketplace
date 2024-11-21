821328921079865b4807b55cf8fee2198e393bfb
usik-sviatoslav <usik.sviatoslav.dev@gmail.com>
2024-11-21 13:10:46 +0200

    The `image` application in `utils` has been updated.
    - Added support for `heic` and `heif` formats for images.
    - Added function of image resize.
    - Removed the `validate_image_resolution` validator.

--- backend/apps/utils/image/tests/test_validators.py
+++ backend/apps/utils/image/tests/test_validators.py
@@ -1,8 +1,8 @@
 import pytest
+from django.core.exceptions import ValidationError
 from PIL import Image
-from rest_framework.exceptions import ValidationError
 
-from apps.utils.image.validators import validate_image_resolution, validate_image_size
+from apps.utils.image.validators import validate_image_size
 
 
 class TestValidators:
@@ -22,15 +22,3 @@ class TestValidators:
         else:  # Waiting for validation error
             with pytest.raises(ValidationError, match="Image file too large"):
                 validate_image_size(self.mock_image)
-
-    @pytest.mark.parametrize("is_valid", (True, False))
-    def test_validate_image_resolution(self, is_valid):
-        # Setting the image resolution
-        self.mock_image.size = (3000, 4000) if is_valid else (3500, 4500)
-
-        if is_valid:  # Validation runs without errors
-            validate_image_resolution(self.mock_image)
-
-        else:  # Waiting for validation error
-            with pytest.raises(ValidationError, match="Image resolution is too high"):
-                validate_image_resolution(self.mock_image)
