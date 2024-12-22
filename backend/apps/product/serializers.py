fbf4af36d555d8ee6eac8bf30c85038037ffc0d0
V-Kovshov <ssevenn41@gmail.com>
2024-12-23 00:16:48 +0200

    Added changes to the product model
    - Removed validator for product quantity
    - Added receiver for image category reset
    - Overridden validate_image function for ProductUpdateSerializer
    - Added asynchronous task to delete product image
    - Changed path url
    - Changed update method for ProductPrivateViewSet

--- backend/apps/product/serializers.py
+++ backend/apps/product/serializers.py
@@ -1,4 +1,5 @@
 import hashlib
+from typing import Optional
 
 from django.core.files.uploadedfile import TemporaryUploadedFile
 from django.utils.translation import gettext_lazy as _
@@ -81,13 +82,17 @@ class ProductCrateSerializer(serializers.ModelSerializer):
 
     @staticmethod
     def validate_images(images: list[TemporaryUploadedFile]) -> list[TemporaryUploadedFile]:
-        """Check uniqueness of images through hashing."""
+        """Check the uniqueness of images through hashing."""
         unique_hashes, unique_images = set(), []
 
+        # Get hashes of uploaded images
         for image in images:
             file_hash = hashlib.md5(b"".join(image.chunks())).hexdigest()
+            image.seek(0)
 
+            # Add image if file hash is unique
             if file_hash not in unique_hashes:
+                image._temp_hash = file_hash
                 unique_hashes.add(file_hash)
                 unique_images.append(image)
 
@@ -95,8 +100,46 @@ class ProductCrateSerializer(serializers.ModelSerializer):
 
 
 class ProductUpdateSerializer(ProductCrateSerializer):
-    pass
+    def validate_images(
+        self, images: list[TemporaryUploadedFile]
+    ) -> list[tuple[str, Optional[ProductImage], int, Optional[TemporaryUploadedFile]]]:
+        """
+        Validates the provided images by ensuring uniqueness and determining
+        the actions needed for each image - create, update, or delete.
 
-    # def validate_images(self, images: list[TemporaryUploadedFile]) -> list[tuple[int, bool, TemporaryUploadedFile]]:
-    #     unique_images = super().validate_images(images)
-    #     pass
+        Args:
+            images (list[TemporaryUploadedFile]): List of uploaded image files.
+
+        Returns:
+            list[tuple]: A list of tuples:
+                - action type,
+                - existing ProductImage object,
+                - image index,
+                - temporary file.
+        """
+        # First validate uniqueness using parent method
+        unique_images = super().validate_images(images)
+        instance_images = {}
+        result = []
+
+        # Get hashes of existing instance images
+        for obj in self.instance.images.all():
+            file_hash = hashlib.md5(b"".join(obj.image_small.chunks())).hexdigest()
+            instance_images[file_hash] = obj
+
+        # Prepare data for each image
+        for index, temp_file in enumerate(unique_images, start=1):
+            file_hash = temp_file._file_hash  # noqa
+
+            # Preparing data for creation
+            if file_hash not in instance_images:
+                result.append(("create", None, index, temp_file))
+
+            # Preparing data for updating
+            elif file_hash in instance_images:
+                obj = instance_images.pop(file_hash)
+                result.append(("update", obj, index, None))
+
+        # Preparing data for deletion (remaining in instance_images)
+        result.extend([("delete", obj, 0, None) for obj in instance_images.values()])
+        return result
---
+++ backend/apps/utils/image/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from .fields import TempImageField
+
+
+class TempImage(models.Model):
+    class Meta:
+        abstract = True
+
+    image_temp = TempImageField()
