fb628091cc81779fac823ea4a65c852a0d060aff
Karlova Tetiana <tanyalyt80@gmail.com>
2025-01-07 13:11:48 +0200

    separation of the metod-delivery

---
+++ frontend/src/components/Cart/DeliveryResult/DeliveryResult.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import css from './DeliveryResult.module.css';
+
+export default function DeliveryResult({ seller }) {
+  const { deliveryData } = useSelector((state) => state.cart);
+
+  return (
+    <div className={css.deliveryItem}>
+      <div className={css.deliveryResultBlock}>
+        {deliveryData[seller].type === 'nova-poshta' && (
+          <>
+            <div className={css.resultbox}>
+              <div className={css.resultInput}></div>
+              <p className={css.resultText}>Доставка Нова Пошта у відділення</p>
+            </div>
+            <p className={css.resnltText}>
+              <b>Адреса відділення:</b>
+              {deliveryData[seller].city}
+            </p>
+            <p className={css.resnltText}>
+              <b>Години роботи</b>
+              {deliveryData[seller].branch}
+            </p>
+          </>
+        )}
+        {deliveryData[seller].type === 'post_box' && (
+          <>
+            <div className={css.resultbox}>
+              <div className={css.resultInput}></div>
+              <p className={css.resultText}>Доставка Нова Пошта у поштомат</p>
+            </div>
+            <p className={css.resnltText}>{deliveryData[seller].city}</p>
+            <p className={css.resnltText}>{deliveryData[seller].postbox}</p>
+          </>
+        )}
+        {deliveryData[seller].type === 'courier' && (
+          <>
+            <div className={css.resultbox}>
+              <div className={css.resultInput}></div>
+              <p className={css.inputText}>Доставка кур’єром Нова Пошта</p>
+            </div>
+            <p className={css.resnltText}>
+              <b> Адреса доставки: </b>&nbsp;
+              {deliveryData[seller].street},&nbsp;
+              {deliveryData[seller].house},&nbsp;
+              {deliveryData[seller].apartment},&nbsp;
+              {deliveryData[seller].city}
+            </p>
+          </>
+        )}
+
+        {deliveryData[seller].type === 'ukrposhta' && (
+          <>
+            <div className={css.resultbox}>
+              <div className={css.resultInput}></div>
+              <p className={css.resultText}>Доставка Нова Пошта у відділення</p>
+            </div>
+            <p className={css.resnltText}>
+              <b> Адреса доставки: </b>&nbsp;
+              {deliveryData[seller].street},&nbsp;
+              {deliveryData[seller].house},&nbsp;
+              {deliveryData[seller].apartment},&nbsp;
+              {deliveryData[seller].city}
+            </p>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
