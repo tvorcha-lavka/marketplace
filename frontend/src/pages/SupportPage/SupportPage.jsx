2e14be1dfe81b54c38ac9f8a42ef42f203f98e8c
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2025-05-31 20:57:48 +0300

    refactor(SupportPage): update navigation flow

--- frontend/src/pages/SupportPage/SupportPage.jsx
+++ frontend/src/pages/SupportPage/SupportPage.jsx
@@ -1,29 +1,14 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import css from './SupportPage.module.css';
 
 export default function SupportPage() {
-  return (
-    <section className="container">
-      <div className={`${css.section} section`}>
-        <h1 className={css.header}>Support Page</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua... Lorem
-          ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua... Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua... Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua... Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua... Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua... Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua... Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua...
-        </p>
-      </div>
-    </section>
-  );
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/coming-soon');
+  }, [navigate]);
+
+  return null;
 }
