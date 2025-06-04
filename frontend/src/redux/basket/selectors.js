90783b47a8ebc3b9aa3697d5c2a2cbad9c0b028c
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2025-06-04 23:51:35 +0300

    refactor(redux basket): update store, slice, selectors

--- frontend/src/redux/basket/selectors.js
+++ frontend/src/redux/basket/selectors.js
@@ -1,12 +1,21 @@
 export const selectBasketItems = (state) => state.basket.items;
+export const selectCustomerData = (state) => state.basket.customerData;
+export const selectDeliveryData = (state) => state.basket.deliveryData;
+export const selectPaymentData = (state) => state.basket.paymentData;
+export const selectDeliveryFee = (state) => state.basket.deliveryFee;
+export const selectCartStep = (state) => state.basket.step;
+export const selectCart = (state) => state.basket;
+export const selectCachedCities = (state) => state.basket.cachedCities;
+export const selectCachedBranches = (state) => state.basket.cachedBranches;
+export const selectCachedPboxCities = (state) => state.basket.cachedPboxCities;
+export const selectCachedPostbox = (state) => state.basket.cachedPostbox;
 
 export const selectTotal = (state) => {
-
   return state.basket.items.reduce((total, item) => {
     const price = parseFloat(item.price);
     if (isNaN(price)) {
       console.error('Non-numeric price found:', item.price);
-      return total; 
+      return total;
     }
     return total + price;
   }, 0);
