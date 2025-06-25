4bc36275d40e91ec034370cd1b5ae0176cfed3a5
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2025-06-25 20:22:05 +0300

    feat(page): created profile settings page

---
+++ frontend/src/pages/ProfileSettingsPage/ProfileSettingsPage.jsx
@@ -0,0 +1,11 @@
+import UserProfileSettings from '../../components/UserProfileSettings/UserProfileSettings';
+
+export default function ProfileSettingsPage() {
+  return (
+    <section className="container">
+      <div className="section">
+        <UserProfileSettings />
+      </div>
+    </section>
+  );
+}
