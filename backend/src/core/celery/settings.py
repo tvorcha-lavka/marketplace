ec3c5a90f3701c8e0c5ef8db682d9fcc7d91c93d
usik-sviatoslav <usik.sviatoslav.dev@gmail.com>
2025-07-01 04:24:09 +0300

    chore(celery): switch broker from rpc to redis

--- backend/src/core/celery/settings.py
+++ backend/src/core/celery/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class CelerySettings(BaseSettings):
     APP_NAME: str = "backend"
     BROKER_URL: str = "pyamqp://admin:admin@rabbitmq:5672//"
-    RESULT_BACKEND: str = "rpc://"
+    RESULT_BACKEND: str = "redis://redis:6379/0"
 
     model_config = SettingsConfigDict(
         env_prefix="CELERY_",
