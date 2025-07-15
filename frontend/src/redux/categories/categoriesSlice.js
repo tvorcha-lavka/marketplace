67553dd139b2915b6aeff69644a8ab152ef92077
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2025-07-15 20:03:54 +0300

    refactor(categories redux): updated the code to fetch the full catalog list in order to implement proper breadcrumbs for search results navigation

--- frontend/src/redux/categories/categoriesSlice.js
+++ frontend/src/redux/categories/categoriesSlice.js
@@ -20,6 +20,7 @@ const categoriesSlice = createSlice({
   initialState: {
     items: [],
     catalog: [],
+    catalogFlat: [],
     popular: [],
     categoryById: {},
     selectedCategoryId: null,
@@ -49,9 +50,11 @@ const categoriesSlice = createSlice({
         state.error = null;
       })
       .addCase(getCategoryById.rejected, handleRejected)
+
       .addCase(getCatalog.pending, handlePending)
       .addCase(getCatalog.fulfilled, (state, action) => {
-        state.catalog = action.payload;
+        state.catalog = action.payload.tree[0].children; 
+        state.catalogFlat = action.payload.flat; 
         state.loading = false;
         state.error = null;
       })
