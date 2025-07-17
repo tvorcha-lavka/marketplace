c561a6d1dcad7cfd88ad278150e73672238c2277
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2025-07-17 13:16:01 +0300

    feat(routes scroll): added scrollToTop for navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,16 @@ import { validateTokensOnPageReload } from './redux/axiosConfig';
 //import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import SharedLayout from './components/SharedLayout/SharedLayout';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import ModalParentComponent from './authModalComponents/ModalParentComponent/ModalParentComponent';
 import SocialAuthHandler from './authModalComponents/SocialAuthHandler/SocialAuthHandler';
 import SessionExpiredModal from './authModalComponents/SessionExpiredModal/SessionExpiredModal';
+
+const SupportPage = lazy(() => import('./pages/SupportPage/SupportPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
+const ComingSoonPage = lazy(
+  () => import('./pages/ComingSoonPage/ComingSoonPage')
+);
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const AllCategoriesPage = lazy(
@@ -51,12 +58,6 @@ const PaymentDeliveryHeaderPage = lazy(
   () => import('./pages/PaymentDeliveryHeaderPage/PaymentDeliveryHeaderPage')
 );
 
-const SupportPage = lazy(() => import('./pages/SupportPage/SupportPage'));
-const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
-const ComingSoonPage = lazy(
-  () => import('./pages/ComingSoonPage/ComingSoonPage')
-);
-
 /*USER PROFILE SETTING PAGES*/
 const ProfileSettingsPage = lazy(
   () => import('./pages/ProfileSettingsPage/ProfileSettingsPage')
@@ -82,6 +83,8 @@ export default function App() {
 
   return (
     <>
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
